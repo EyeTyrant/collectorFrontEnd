8155fdfe09063eaa6be341c15e8df60fdea3f196
EyeTyrant <1414jg@gmail.com>
2020-02-17 14:51:18 -0600

    added reactiveFormsModule and implemented formGroup. tested input with json pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { InputComponent } from './input/input.component';
-import { DisplayComponent } from './display/display.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { InputComponent } from "./input/input.component";
+import { DisplayComponent } from "./display/display.component";
 
 @NgModule({
   declarations: [
@@ -13,10 +14,8 @@ import { DisplayComponent } from './display/display.component';
     InputComponent,
     DisplayComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-input',
-  templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  selector: "app-input",
+  templateUrl: "./input.component.html",
+  styleUrls: ["./input.component.css"]
 })
 export class InputComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  dieCastInputForm = new FormGroup({
+    releaseYear: new FormControl(""),
+    name: new FormControl(""),
+    brand: new FormControl(""),
+    manufacturer: new FormControl("")
+  });
 }
