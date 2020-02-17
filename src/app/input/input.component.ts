ec2ab2e5115e3d92386cc878d057a39aa52a1bec
EyeTyrant <1414jg@gmail.com>
2020-02-17 15:10:21 -0600

    Refactored to use FormBuilder

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+// import { FormGroup, FormControl } from "@angular/forms"; // replaced by formBuilder
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: "app-input",
@@ -7,14 +8,23 @@ import { FormGroup, FormControl } from "@angular/forms";
   styleUrls: ["./input.component.css"]
 })
 export class InputComponent implements OnInit {
-  constructor() {}
+  constructor(private frmBldr: FormBuilder) {}
 
   ngOnInit() {}
 
-  dieCastInputForm = new FormGroup({
-    releaseYear: new FormControl(""),
-    name: new FormControl(""),
-    brand: new FormControl(""),
-    manufacturer: new FormControl("")
+  dieCastInputForm = this.frmBldr.group({
+    releaseYear: [""],
+    name: [""],
+    brand: [""],
+    manufacturer: [""]
   });
+
+  // added to constructor parameters and replaced by formBuilder
+
+  // dieCastInputForm = new FormGroup({
+  //   releaseYear: new FormControl(""),
+  //   name: new FormControl(""),
+  //   brand: new FormControl(""),
+  //   manufacturer: new FormControl("")
+  // });
 }
